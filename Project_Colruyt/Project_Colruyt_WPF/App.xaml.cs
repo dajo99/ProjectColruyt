d409da9fa861abcc01e4a36df008ac410f6f658c
Dajo Vandoninck <dajo.vandoninck99@gmail.com>
2020-10-22 14:01:01 +0200

    fix

---
+++ Project_Colruyt/Project_Colruyt_WPF/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using Project_Colruyt_WPF.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Runtime.Remoting.Channels;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Project_Colruyt_WPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+         private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            MainViewModel viewmodel = new MainViewModel();
+            Views.MainView view = new Views.MainView();
+            view.DataContext = viewmodel;
+            view.Show();
+
+        }
+    }
+}
