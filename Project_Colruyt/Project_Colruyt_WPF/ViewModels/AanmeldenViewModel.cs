19422bf6549de5f3b157e82aef4afacf835b78db
Mitch Van Rechem <r0597894@student.thomasmore.be>
2020-11-18 01:15:53 +0100

    Login

---
+++ Project_Colruyt/Project_Colruyt_WPF/ViewModels/AanmeldenViewModel.cs
@@ -0,0 +1,75 @@
+﻿using MongoDB.Driver;
+using Project_Colruyt_DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Project_Colruyt_DAL.DatabaseOperations;
+
+namespace Project_Colruyt_WPF.ViewModels
+{
+    class AanmeldenViewModel: BasisViewModel
+    {
+        IMongoCollection<Gebruikers> collection = DatabaseOperations.GetUsers();
+
+        private string _email;
+        private string _wachtwoord;
+
+        public string Email
+        {
+            get { return _email; }
+            set {
+                _email = value;
+                NotifyPropertyChanged();
+            }
+        }
+        public string Wachtwoord
+        {
+            get { return _wachtwoord; }
+            set {
+                _wachtwoord = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+       
+        public void Authenticeer()
+        {
+            
+        }
+
+        public override string this[string columnName]
+        {
+            get
+            {
+                if (columnName == "Email" && string.IsNullOrWhiteSpace(Email))
+                {
+                    return "Vul een geldig emailadres in!";
+                }
+                else if (columnName == "Wachtwoord" && string.IsNullOrWhiteSpace(Email))
+                {
+                    return "Vul een wachtwoord in om aan te melden!";
+                }
+
+                return "";
+            }
+        }
+
+
+
+
+        public override bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public override void Execute(object parameter)
+        {
+            switch (parameter.ToString())
+            {
+                case "Aanmelden": Authenticeer(); break;
+            }
+        }
+    }
+}
