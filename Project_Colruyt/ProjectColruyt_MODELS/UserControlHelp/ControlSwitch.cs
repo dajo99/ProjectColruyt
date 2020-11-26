3bc6f17b84aacb0b542e14521ccfc6854bcea7ee
Mitch Van Rechem <r0597894@student.thomasmore.be>
2020-11-26 10:43:29 +0100

    tested decoding

--- Project_Colruyt/ProjectColruyt_MODELS/UserControlHelp/ControlSwitch.cs
+++ Project_Colruyt/ProjectColruyt_MODELS/UserControlHelp/ControlSwitch.cs
@@ -8,15 +8,22 @@ using System.Windows.Controls;
 namespace ProjectColruyt_MODELS.UserControlHelp
 {
     public delegate void ColruytDelegate(UserControl usc, string title);
+    //public delegate void ButtonVisibilityEvent(string visibility);
 
     public static class ControlSwitch
     {
         public static event ColruytDelegate UscEvent;
+        //public static event ButtonVisibilityEvent ButtonVisibilityEvent;
 
         public static void InvokeSwitch(UserControl usc, string title)
         {
             UscEvent?.Invoke(usc, title);
         }
- 
+
+        //public static void SwitchVisibility(string visibility)
+        //{
+        //    ButtonVisibilityEvent?.Invoke(visibility);
+        //}
+
     }
 }
--- Project_Colruyt/Project_Colruyt_WPF/ViewModels/AanmeldenViewModel.cs
+++ Project_Colruyt/Project_Colruyt_WPF/ViewModels/AanmeldenViewModel.cs
@@ -1,4 +1,5 @@
-﻿using MongoDB.Driver;
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
 using Project_Colruyt_DAL;
 using Project_Colruyt_WPF.Views;
 using ProjectColruyt_MODELS;
@@ -59,26 +60,41 @@ namespace Project_Colruyt_WPF.ViewModels
         public void Authenticeer()
         {
             gebruiker = DatabaseOperations.GetUserByEmail(Email);
+            string result = DecodePassword(gebruiker.password);
 
             if (gebruiker == null)
             {
                 Melding = "Een gebruiker met dit emailadres bestaat niet!";
             }
-            else if (gebruiker.password != Wachtwoord)
+            else if (Wachtwoord != result)
             {
                 Melding = "Fout wachtwoord!";
             }
             else
             {
+
                 GebruikerStatic.Gebruiker = gebruiker;
+               
                 Usercontrols.LijstOverzicht_usercontrol usc = new Usercontrols.LijstOverzicht_usercontrol();
                 usc.DataContext = new LijstOverzichtViewModel();
                 ControlSwitch.InvokeSwitch(usc, "Winkellijsten");
+                //ControlSwitch.SwitchVisibility("Visible");
             }
 
         }
 
-        
+        public string DecodePassword(BsonString encodedData)
+        {
+            System.Text.UTF8Encoding encoder = new System.Text.UTF8Encoding();
+            System.Text.Decoder utf8Decode = encoder.GetDecoder();
+            byte[] todecode_byte = Convert.FromBase64String(encodedData.ToString());
+            int charCount = utf8Decode.GetCharCount(todecode_byte, 0, todecode_byte.Length);
+            char[] decoded_char = new char[charCount];
+            utf8Decode.GetChars(todecode_byte, 0, todecode_byte.Length, decoded_char, 0);
+            string result = new String(decoded_char);
+            return result;
+        }
+
 
         public override string this[string columnName]
         {
